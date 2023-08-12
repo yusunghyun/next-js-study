5e961aa3764ce49222febeb190605ad7164db191
yusunghyun <ysh1513@naver.com>
2023-08-12 18:06:27 +0900

    ✨ 4.2) fetch data : REST API with ServerComponent

---
+++ src/app/my/page.tsx
@@ -0,0 +1,20 @@
+async function callApi() {
+  const userReq = await fetch("https://jsonplaceholder.typicode.com/users");
+  let result = await userReq.json();
+  //   console.log("result : ", result);
+  return result;
+}
+
+export default async function Page() {
+  const userReq = await callApi();
+
+  console.log("userReq : ", userReq.length);
+
+  return (
+    <div>
+      {userReq.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </div>
+  );
+}
