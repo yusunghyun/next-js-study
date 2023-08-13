2c28730ad2e8406f8436b88c77394d43106a0ee5
yusunghyun <ysh1513@naver.com>
2023-08-13 21:55:47 +0900

    test 2

--- src/app/my/ThemeSwitch.tsx
+++ src/app/my/ThemeSwitch.tsx
@@ -4,11 +4,11 @@ import { useTheme } from "next-themes";
 
 export const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme();
-  const darkMain = theme === "dark";
+  const darkMain2 = theme === "dark";
 
-  console.log("dark : ", darkMain);
+  console.log("dark : ", darkMain2);
 
-  const toggleTheme = () => setTheme(darkMain ? "light" : "dark");
+  const toggleTheme = () => setTheme(darkMain2 ? "light" : "dark");
 
   return (
     <button
